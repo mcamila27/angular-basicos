8217f699c90b008518ba2a0a03a082cc2ef9cd79
Duenas <maria.duenas@softwareone.com>
2022-04-12 17:29:47 -0500

    Primer commit

---
+++ src/app/app.module.ts
@@ -0,0 +1,20 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppComponent } from './app.component';
+import { HeoresModule } from './heroes/heroes.module';
+import { ContadorModule } from './contador/Contador.module';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+  ],
+  imports: [
+    BrowserModule, 
+    HeoresModule,
+    ContadorModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
